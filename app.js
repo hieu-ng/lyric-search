ba0554a7a4d127fe67089f2b77cb2d79e15016ae
hieu-ng <hieutnguyen06@gmail.com>
2020-06-02 10:28:43 +1000

    Add API

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
