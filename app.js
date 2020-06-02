572ffcc4804b017079df1247560788dca70a55c2
hieu-ng <hieutnguyen06@gmail.com>
2020-06-02 10:30:55 +1000

    Search for song lyrics

--- app.js
+++ app.js
@@ -54,3 +54,17 @@ async function getMoreSongs(url) {
 
 	showData(data);
 }
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+	const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+	const data = await res.json();
+
+	const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+	result.innerHTML =
+		`<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+	more.innerHTML = '';
+}
