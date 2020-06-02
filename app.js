6d856f756e5a3b47e413b7b074abc6bcd591ad8a
hieu-ng <hieutnguyen06@gmail.com>
2020-06-02 10:29:33 +1000

    Use API to search for songs or artists

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const result = document.getElementById('result');
 const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
+
+async function searchSongs(term) {
+	const res = await fetch(`${apiURL}/suggest/${term}`);
+	const data = await res.json();
+
+	showData(data);
+}
